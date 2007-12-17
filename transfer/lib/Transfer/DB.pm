184e72c4eba8cb1feff9d9a07bac406fdbfea3a7
Adam Turoff <aturoff@rdc.lctl.gov>
2007-12-17 23:04:31 +0000

    Allow the database configuration details to live in etc/transfer.conf, a simple file of 'name=value' pairs.
    Closes #10
    
    
    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/trunk@47 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- transfer/lib/Transfer/DB.pm
+++ transfer/lib/Transfer/DB.pm
@@ -2,6 +2,9 @@ package Transfer::DB;
 
 use DBI;
 
+use File::Basename;
+use Cwd qw(abs_path);
+
 require Exporter;
 @ISA=qw(Exporter);
 @EXPORT=qw(dbhandle);
@@ -10,13 +13,35 @@ require Exporter;
 
 my $dbh;
 
+sub get_connection_params() {
+    my $conf = abs_path(dirname($0) . "/../etc/transfer.conf");
+    my %config;
+    
+    if (-e $conf) {
+        open(my $fh, $conf);
+        local $/ = undef;
+        my $input = <$fh>;
+        %config = $input =~ m/^[ \t]*(\S+)[ \t]*=[ \t]*(.*?)[ \t]*$/mg;
+        close($fh);
+    }
+
+    ## Use reasonable defaults
+    $config{dbname}   ||= "transfer";
+    $config{username} ||= ""        ;
+    $config{password} ||= ""        ;
+    
+    my $connect = "dbi:Pg:dbname=$config{dbname}";
+    return ($connect, $config{username}, $config{password});
+}
+
 sub dbhandle {
 	return $dbh if defined $dbh;
 
+    my ($connect, $user, $pass) = get_connection_params();
 	my $params = {PrintError => $ENV{DBIERROR} + 0};
 
-	$dbh = DBI->connect("dbi:Pg:dbname=transfer", "", "", $params)
-				or die "FATAL: Cannot connect to transfer database. $!\n";
+	$dbh = DBI->connect($connect, $user, $pass, $params)
+		or die "FATAL: Cannot connect to transfer database. $!\n";
 
 	return $dbh;
 }
