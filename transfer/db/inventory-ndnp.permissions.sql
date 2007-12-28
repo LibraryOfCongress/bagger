089824d42cfd309a96a47569bf041b793f6d0169
Justin Littman <jlit@loc.gov>
2007-12-28 13:21:59 +0000

    Added permissions sql.
    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/trunk@71 b77ddd6a-a23c-0410-94b0-b3c43762c485

---
+++ transfer/db/inventory-ndnp.permissions.sql
@@ -0,0 +1,13 @@
+GRANT USAGE ON SCHEMA ndnp TO package_modeler_data_writer_role;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE ndnp.batch TO GROUP package_modeler_data_writer_role;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE ndnp.batch_lccn TO GROUP package_modeler_data_writer_role;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE ndnp.batch_reel TO GROUP package_modeler_data_writer_role;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE ndnp.lccn TO GROUP package_modeler_data_writer_role;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE ndnp.reel TO GROUP package_modeler_data_writer_role;
+
+GRANT USAGE ON SCHEMA ndnp TO package_modeler_reader_role;
+GRANT SELECT ON TABLE ndnp.batch TO GROUP package_modeler_reader_role;
+GRANT SELECT ON TABLE ndnp.batch_lccn TO GROUP package_modeler_reader_role;
+GRANT SELECT ON TABLE ndnp.batch_reel TO GROUP package_modeler_reader_role;
+GRANT SELECT ON TABLE ndnp.lccn TO GROUP package_modeler_reader_role;
+GRANT SELECT ON TABLE ndnp.reel TO GROUP package_modeler_reader_role;
