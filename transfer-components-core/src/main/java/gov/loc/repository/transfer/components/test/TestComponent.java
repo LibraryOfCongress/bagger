82040a826becf1e46ff0f0671d4059a3fbc2f53a
Justin Littman <jlit@loc.gov>
2008-01-16 15:58:08 +0000

    For ndnp-transfer 30.
    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/trunk@97 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- transfer-components-core/src/main/java/gov/loc/repository/transfer/components/test/TestComponent.java
+++ transfer-components-core/src/main/java/gov/loc/repository/transfer/components/test/TestComponent.java
@@ -9,6 +9,7 @@ public interface TestComponent extends Component {
 	@JobType(name="test")
 	public void test(
 			@MapParameter(name="message") String message,
-			@MapParameter(name="istrue") boolean isTrue
+			@MapParameter(name="istrue") boolean isTrue,
+			@MapParameter(name="key") long key
 	) throws Exception;
 }
