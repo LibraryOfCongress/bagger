7075ff34c29241f2941b4d2bff4e2e323f7c3694
Andy Boykono <aboyko@rdc.lctl.gov>
2008-04-08 22:22:14 +0000

    rename again, and comment a little, and support BagIt fetch.txt
    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/trunk@317 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- utilities/parallelretriever.py
+++ utilities/parallelretriever.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
-# ia-acquisition.py
+""" This "parallel retriever" takes as input a "file manifest" and 
+   "retrieval order" for a remotely available BagIt package, and 
+    launches a specified number of parallel instances of an appropriate
+    retriever tool (rsync, wget) in order to retrieve the contents of
+    the package.  It produces a BagIt package as output. """
 
 import os, sys, shutil, time
 from optparse import OptionParser
@@ -12,6 +16,7 @@ def generate_package_identifier():
 
 
 def get_options_and_args():
+    """Collect the command-line arguments from optparse"""
     parser = OptionParser()
     parser.add_option("-n", "--number-of-processes", dest="num_processes", 
                       type="int", default=16, 
@@ -42,18 +47,23 @@ def get_options_and_args():
 
 
 def retrieve_package(options):
+    """Fork the desired number of retriever processes to 
+       work on getting the files"""
     package_directory = os.path.join(options.destination_path,
                                      options.package_identifier)
     os.mkdir(package_directory)
 
-    shutil.copy(options.file_manifest, package_directory)
+    shutil.copy(options.file_manifest,   package_directory)
+    shutil.copy(options.retrieval_order, package_directory)
                     
     # distribute the contents of the retrieval order file into 
     # num_processes buckets, round-robin-like.
-    retrieval_orders = ([],) * options.num_processes
+    retrieval_orders = tuple([ [] for i in range(options.num_processes) ])
     counter = 0
     for line in file(options.retrieval_order).readlines():
-        retrieval_orders[counter % options.num_processes].append(tuple(line.strip().split()))
+        retrieval_item = tuple(line.strip().split())
+        bucket_number  = counter % options.num_processes
+        retrieval_orders[bucket_number].append(retrieval_item)
         counter = counter + 1
 
     logfile = file(os.path.join(package_directory, "retrieval.log"), "w")
@@ -61,18 +71,27 @@ def retrieve_package(options):
     for i in range(options.num_processes):
         pid = os.fork()
         if not pid:
-            print "working on %d items in list %d" % (len(retrieval_orders[i]), i)
-            for url, filename in retrieval_orders[i]:
+            print "working on %d items in list %d" % (
+                   len(retrieval_orders[i]), i)
+            for items in retrieval_orders[i]:
+                # tolerate older syntax for fetch files, with two tokens
+                if len(items) == 2: 
+                    url, filename = items
+                else:
+                    url, filesize, filename = items
                 filename = os.path.join(package_directory, filename)
                 try:
                     os.makedirs(os.path.dirname(filename))
                 except OSError:
+                    # it's OK if the directories are already there
                     pass
                 if url.startswith('rsync'):
-                    ret = os.spawnlp(os.P_WAIT, "rsync", "rsync", "-ar", url, filename)
+                    ret = os.spawnlp(os.P_WAIT, "rsync", "rsync", "-ar", 
+                                     url, filename)
                 else:
-                    ret = os.spawnlp(os.P_WAIT, "wget", "wget", "-q", "-O", filename, url)
-                logfile.write("%d: %s\n" % (ret, filename))
+                    ret = os.spawnlp(os.P_WAIT, "wget", "wget", "-q", "-O", 
+                                     filename, url)
+                logfile.write("%s %s %d\n" % (time.asctime(), filename, ret))
             sys.exit(0)
         else:
             pids.append(pid)
@@ -83,6 +102,6 @@ def retrieve_package(options):
 
 
 if __name__ == '__main__':
-    options, args = get_options_and_args()
+    (options, args) = get_options_and_args()
     retrieve_package(options)
 
