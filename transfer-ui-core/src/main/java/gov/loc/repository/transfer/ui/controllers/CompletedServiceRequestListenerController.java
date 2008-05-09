ee1bc3529cde53db7ac1395dfd8cb3a78a54976e
Justin Littman <jlit@loc.gov>
2008-05-09 12:07:29 +0000

    Missed commits.
    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/trunk@377 b77ddd6a-a23c-0410-94b0-b3c43762c485

---
+++ transfer-ui-core/src/main/java/gov/loc/repository/transfer/ui/controllers/CompletedServiceRequestListenerController.java
@@ -0,0 +1,73 @@
+package gov.loc.repository.transfer.ui.controllers;
+
+import gov.loc.repository.transfer.ui.UIConstants;
+import gov.loc.repository.transfer.ui.utilities.PermissionsHelper;
+import gov.loc.repository.workflow.continuations.CompletedServiceRequestListener;
+import gov.loc.repository.workflow.continuations.CompletedServiceRequestListener.State;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CompletedServiceRequestListenerController
+{
+	
+	
+	private static final Log log = LogFactory.getLog(CompletedServiceRequestListenerController.class);
+	private CompletedServiceRequestListener listener;
+	
+	@Autowired
+	@Required
+	public void setCompletedServicerRequestListener(CompletedServiceRequestListener listener)
+	{
+		this.listener = listener;
+	}
+	
+	@PostConstruct
+	public void init()
+	{
+		this.listener.start();
+	}
+	
+	@SuppressWarnings("unchecked")
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView handleGet(HttpServletRequest req) throws Exception {
+		PermissionsHelper permissions = new PermissionsHelper(req);
+		
+		ModelAndView mav = new ModelAndView("completedjoblistener");
+		mav.addObject("listener", this.listener);
+		mav.addObject("permissions", permissions);
+		return mav;		
+	}
+
+	@RequestMapping(method=RequestMethod.POST)
+	public ModelAndView handlePost(HttpServletRequest req) throws Exception {
+		try
+		{
+			if (req.getParameter("start") != null && this.listener.getState() == State.STOPPED) {
+				this.listener.start();
+				req.getSession().setAttribute(UIConstants.SESSION_MESSAGE, "Started Job Listener");
+			}
+			else if (req.getParameter("stop") != null && this.listener.getState() == State.STARTED) {
+				this.listener.stop();
+				req.getSession().setAttribute(UIConstants.SESSION_MESSAGE, "Stopped Job Listener");
+			}
+		}
+		catch(Exception ex)
+		{
+			log.error("Error thrown starting/stop CompletedServiceRequestListener", ex);
+			req.getSession().setAttribute(UIConstants.SESSION_MESSAGE, "Unable to start/stop Job Listener");
+		}
+		return this.handleGet(req);
+	}
+	
+}
---
+++ transfer-ui-core/src/main/java/gov/loc/repository/transfer/ui/controllers/ServiceRequestBrokerController.java
@@ -0,0 +1,42 @@
+package gov.loc.repository.transfer.ui.controllers;
+
+import gov.loc.repository.serviceBroker.dao.ServiceRequestDAO;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ServiceRequestBrokerController
+{
+	
+	
+	private static final Log log = LogFactory.getLog(ServiceRequestBrokerController.class);
+	private ServiceRequestDAO dao;
+	
+	@Resource(name="serviceBroker")
+	@Required
+	public void setServiceRequestDAO(ServiceRequestDAO dao)
+	{
+		this.dao = dao;
+	}
+		
+	@SuppressWarnings("unchecked")
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView handleGet(HttpServletRequest req) throws Exception {
+		//PermissionsHelper permissions = new PermissionsHelper(req);
+		
+		ModelAndView mav = new ModelAndView("servicerequestbroker");
+		mav.addObject("serviceRequestList", this.dao.findServiceRequests(true, false, false));
+		//mav.addObject("permissions", permissions);
+		return mav;		
+	}
+	
+}
