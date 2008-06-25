5a3f390a676207c5e37aa5020f6546d0d6410f1c
Daniel Krech <dkre@loc.gov>
2008-06-25 16:49:24 +0000

    added a bit more logging and progress reporting; upgraded to subprocess module for spawning processes; switched to using threads instead of forked processes for managing spawned processes
    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/trunk@516 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- utilities/parallelretriever.py
+++ utilities/parallelretriever.py
@@ -1,23 +1,90 @@
 #!/usr/bin/env python
-""" This "parallel retriever" takes as input a "file manifest" and 
-   "retrieval order" for a remotely available BagIt package, and 
-    launches a specified number of parallel instances of an appropriate
-    retriever tool (rsync, wget) in order to retrieve the contents of
-    the package.  It produces a BagIt package as output. If not supplied,
-    the name of the package will default to the current timestamp (seconds 
-    since Dec 31 1969).
+""" 
+This "parallel retriever" takes as input a "file manifest" and
+"retrieval order" for a remotely available BagIt package, and launches
+a specified number of parallel instances of an appropriate retriever
+tool (rsync, wget) in order to retrieve the contents of the package.
+It produces a BagIt package as output. If not supplied, the name of
+the package will default to the current timestamp (seconds since Dec
+31 1969).
     
-    rsync and wget must be available in the $PATH. 
+rsync and wget must be available in the $PATH. 
 
-    If an rsync password is necessary, it must be supplied via the environment,
-    as in:
-      RSYNC_PASSWORD=pass ./parallelretriever.py -m manifest-md5.txt -r fetch.txt 
+If an rsync password is necessary, it must be supplied via the environment,
+as in:
+  RSYNC_PASSWORD=pass ./parallelretriever.py -m manifest-md5.txt -r fetch.txt 
 
-    Run with '-h' for documentation.
+Run with '-h' for documentation.
 """
 
-import os, sys, shutil, time
+import os
+import sys
+
+if sys.version_info < (2,5,0):
+    print """\
+WARNING: this script is being developed and tested against Python 2.5.x.
+  And you're running with Python version %s
+""" % sys.version
+
+import time
+import Queue
+import shutil
+import urllib2
+import logging
+import threading
+import subprocess
+
 from optparse import OptionParser
+
+
+class ProgressReporter(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        total = 0
+        t0 = time.time()
+        while True:
+            filename = self.queue.get()
+            try:
+                mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime = os.stat(filename)
+                total += size
+                duration = (time.time() - t0)
+                m_bytes_per_second = total / duration / 1024. / 1024.
+                logging.info("fetched %u bytes in %.4g seconds. %.4g MBs (overall)" % 
+                             (total, duration, m_bytes_per_second))
+            finally:
+                self.queue.task_done()
+
+
+finished_queue = Queue.Queue()
+t = ProgressReporter(finished_queue)
+t.setName("Progress Reporter")
+t.setDaemon(True)
+t.start()
+
+
+class FetchWorker(threading.Thread):
+    """
+    Worker to fetch items in fetch.txt
+    """
+    def __init__(self, queue, package_directory):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.package_directory = package_directory
+
+    def run(self):
+        while True:
+            item = self.queue.get()
+            try:
+                url, filesize, filename = item
+                filename = os.path.join(self.package_directory, filename)
+                fetch(filename, url)
+            finally:
+                self.queue.task_done()
+            logging.debug("size: %s" % self.queue.qsize())
+
 
 def generate_package_identifier():
     """Assume no more than one of these packages per second, and 
@@ -25,9 +92,45 @@ def generate_package_identifier():
     return str(int(time.time()))
 
 
+def fetch(filename, url):
+    logging.debug("%s fetching: %s" % (threading.currentThread().getName(), url))
+    try:
+        os.makedirs(os.path.dirname(filename))
+    except OSError:
+        pass # it's OK if the directories are already there
+    if url.startswith('http') or url.startswith('https'):
+        ret = subprocess.call(
+            ["wget", "wget", "-q", "-O", filename, url])
+    elif url.startswith('rsync'):
+        ret = subprocess.call(
+            ["rsync", "rsync", "-ar", url, filename])
+    elif url.startswith('file'):
+        url = urllib2.urlopen(url)
+        f = file(filename, "wb")
+        while True:
+            d = url.read(1024)
+            if d:
+                f.write(d)
+            else:
+                f.close()
+                break
+        ret = 0
+    else:
+        raise Exception("unexpected url type")
+    if ret==0:
+        finished_queue.put(filename)
+    elif ret<0:
+        logging.error("child process was terminated by signal %s" % -ret)
+        return ret
+    else:
+        logging.error("Error in child process %s" % ret)        
+        return ret
+
+
 def retrieve_package(options):
-    """Fork the desired number of retriever processes to 
-       work on getting the files"""
+    """
+    Retrieve the package using the desired number of workers.
+    """
     package_directory = os.path.join(options.destination_path,
                                      options.package_identifier)
     if not os.path.isdir(package_directory):
@@ -36,49 +139,29 @@ def retrieve_package(options):
     shutil.copy(options.file_manifest,   package_directory)
     shutil.copy(options.retrieval_order, package_directory)
                     
-    # distribute the contents of the retrieval order file into 
-    # num_processes buckets, round-robin-like.
-    retrieval_orders = tuple([ [] for i in range(options.num_processes) ])
-    counter = 0
-    for line in file(options.retrieval_order).readlines():
-        retrieval_item = tuple(line.strip().split())
-        bucket_number  = counter % options.num_processes
-        retrieval_orders[bucket_number].append(retrieval_item)
-        counter = counter + 1
+    queue = Queue.Queue()
 
-    logfile = file(os.path.join(package_directory, "retrieval.log"), "w")
-    pids = []
+    # spawn a pool of worker threads
     for i in range(options.num_processes):
-        pid = os.fork()
-        if pid:
-            pids.append(pid)
-        else:
-            print "working on %d items in list %d" % (
-                   len(retrieval_orders[i]), i)
-            for items in retrieval_orders[i]:
-                # tolerate older syntax for fetch files, with two tokens
-                if len(items) == 2: 
-                    url, filename = items
-                else: # bagit spec requires three items in fetch.txt
-                    url, filesize, filename = items
-                filename = os.path.join(package_directory, filename)
-                try:
-                    os.makedirs(os.path.dirname(filename))
-                except OSError:
-                    # it's OK if the directories are already there
-                    pass
-                if url.startswith('rsync'):
-                    ret = os.spawnlp(os.P_WAIT, "rsync", "rsync", "-ar", 
-                                     url, filename)
-                else: # assume wget can handle all other protocols
-                    ret = os.spawnlp(os.P_WAIT, "wget", "wget", "-q", "-O", 
-                                     filename, url)
-                logfile.write("%s %s %d\n" % (time.asctime(), filename, ret))
-            sys.exit(0)
-       
-    for i in range(options.num_processes):
-        pid, exit_status = os.wait()
-        print "%d finished with status %d" % (pid, exit_status)
+        t = FetchWorker(queue, package_directory)
+        t.setName("Fetch Worker #%d" % i)
+        t.setDaemon(True)
+        t.start()
+
+    # populate queue with fetch items
+    for line in file(options.retrieval_order).readlines():
+        parts = line.strip().split()
+        if len(parts)==0:
+            logging.warning("skipping over blank line")
+            continue
+        if len(parts)!=3:
+            logging.error("line does not have three parts as expected. Line: '%s'" % line)
+            sys.exit(-1)
+        item = tuple(parts)
+        queue.put(item)
+
+    #wait on the queue until everything has been processed     
+    queue.join()
 
 
 if __name__ == '__main__':
@@ -108,5 +191,11 @@ if __name__ == '__main__':
     if options.destination_path is None:
         options.destination_path = os.getcwd()
 
-    retrieve_package(options)
+    logging.basicConfig(level=logging.INFO, filename='%s-retrieval.log' % options.package_identifier)
 
+    try:
+        retrieve_package(options)
+    except KeyboardInterrupt, ki:
+        logging.info("Bye Bye")
+    except Exception, e:
+        logging.exception(e)
