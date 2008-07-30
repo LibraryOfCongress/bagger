918009aa891b8c5fedba208410a0d0d37d589781
Michael Giarlo <mgia@rdc.lctl.gov>
2008-07-30 19:32:42 +0000

    added sun29 to ndiipp-prod-main
    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/trunk@572 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- transfer-deployment/ndiipp-prod-main.py
+++ transfer-deployment/ndiipp-prod-main.py
@@ -6,24 +6,24 @@
 from transfer.core import workflow, broker, package as core_package
 
 config = {
-    'DEBUG': False, # If True, will print out actions rather than take them (e.g., will not hit database)
-    'PSQL': '/usr/bin/psql', # Tell me where to find psql (default = '/usr/bin/psql')
+    'DEBUG': True, # If True, will print out actions rather than take them (e.g., will not hit database)
+    'PSQL': '/usr/postgres8.2/bin/psql', # Tell me where to find psql (default = '/usr/bin/psql')
     'PGHOST': 'localhost', # This is the host that the PostgreSQL database lives on (default = localhost)
     'PGPORT': '5432', # This is the port that PostgreSQL listens on (default = 5432)
-    'PGUSER': 'postgres', # This is a username on PostgreSQL with SUPERUSER privlidges (default = postgres)
+    'PGUSER': 'bvar', # This is a username on PostgreSQL with SUPERUSER privlidges (default = postgres)
     'PGPASSWORD': '', # This is the password for the user specified above (default = '')
-    'DB_PREFIX': '', # This will prepend a custom prefix to the database name that will get created.  An _ will be appended. (default = '')
-    'ROLE_PREFIX': '', # This will prepend a custom prefix to the roles that will get created.  An _ will be appended. (default = '')
-    'INSTALL_DIR': 'TBD', # Set the directory that the CLI tools will be unzipped to (default = '.')
+    'DB_PREFIX': 'mgiatest', # This will prepend a custom prefix to the database name that will get created.  An _ will be appended. (default = '')
+    'ROLE_PREFIX': 'mgiatest', # This will prepend a custom prefix to the roles that will get created.  An _ will be appended. (default = '')
+    'INSTALL_DIR': '/export/home/mgia/transfer-bin', # Set the directory that the CLI tools will be unzipped to (default = '.')
     'VERSION': '1.5', # This is the version of the release being deployed
-    'SQL_FILES_LOCATION': 'TBD', #Set the location of the sql files (default = '')
+    'SQL_FILES_LOCATION': '/export/home/mgia/transfer-sql', #Set the location of the sql files (default = '')
     'TRANSFER_PASSWD': '', # Set a password for the package modeler user role (default = 'transfer_user')
 }
 
 core_modeler = core_package.PackageModeler(config)
 
 # Uncomment to drop dbs and roles
-#core_modeler.drop()
+core_modeler.drop()
 
 # Uncomment to create dbs, roles, load with fixtures
 core_modeler.create_database()
@@ -40,5 +40,6 @@ core_modeler.create_fixtures(fixtures=(
         'createrole -id storage_system',
         'createsystem -id sun9 -host sun9.loc.gov -roles storage_system',
         'createsystem -id rs25 -host rs25.loc.gov -roles storage_system',
+	'createsystem -id sun29 -host sun29.loc.gov -roles storage_system'
        ))
 
