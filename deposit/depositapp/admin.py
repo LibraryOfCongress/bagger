13cd496ded039715d2d5e0bc59802120d2ca64bd
Justin Littman <jlit@loc.gov>
2008-10-06 11:28:04 +0000

    Moving deposit
    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/trunk@720 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- deposit/depositapp/admin.py
+++
@@ -1,4 +0,0 @@
-from deposit.depositapp.models import Project, User, NetworkTransfer, ShipmentTransfer
-from django.contrib import admin
-
-admin.site.register((Project,User,NetworkTransfer,ShipmentTransfer))
