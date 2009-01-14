e80eed8cfe0045c3a1aa015b9ee18fba8dc9ed1b
Jonathin Steinbach <jste@rdc.lctl.gov>
2009-01-14 20:17:26 +0000

    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/bagger/trunk@1150 b77ddd6a-a23c-0410-94b0-b3c43762c485

---
+++ bagger-gui/src/main/java/gov/loc/repository/bagger/BaggerLogin.java
@@ -0,0 +1,27 @@
+
+package gov.loc.repository.bagger;
+
+import gov.loc.repository.bagger.bag.BaggerBag;
+
+import org.acegisecurity.Authentication;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.richclient.application.Application;
+import org.springframework.richclient.application.ApplicationWindow;
+import org.springframework.richclient.application.config.ApplicationWindowConfigurer;
+import org.springframework.richclient.command.ActionCommand;
+import org.springframework.richclient.security.LoginCommand;
+
+/**
+ * Custom application lifecycle implementation that configures the app at well defined points within
+ * its lifecycle.
+ *
+ * @author Jon Steinbach
+ */
+public class BaggerLogin extends LoginCommand
+{
+	public void doExecuteCommand() {
+		super.doExecuteCommand();
+		System.out.println("BaggerLogin.doExecuteCommand");
+	}
+}
