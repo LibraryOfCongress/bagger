f017c4c375086c6f20636e1d5775df02da8c2514
Jonathin Steinbach <jste@rdc.lctl.gov>
2009-04-01 20:26:08 +0000

    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/bagger/trunk@1402 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- bagger-gui/src/main/java/gov/loc/repository/bagger/ui/OrganizationInfoForm.java
+++ bagger-gui/src/main/java/gov/loc/repository/bagger/ui/OrganizationInfoForm.java
@@ -2,6 +2,8 @@
 package gov.loc.repository.bagger.ui;
 
 import javax.swing.JComponent;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
 import java.awt.Dimension;
 
 import org.springframework.binding.form.FormModel;
@@ -11,7 +13,7 @@ import org.springframework.richclient.form.builder.TableFormBuilder;
 public class OrganizationInfoForm extends AbstractForm {
     public static final String INFO_FORM_PAGE = "infoPage";
 
-    private JComponent infoField;
+    private JComponent infoForm;
     private Dimension dimension = new Dimension(400, 370);
     private BagView bagView;
 
@@ -21,41 +23,76 @@ public class OrganizationInfoForm extends AbstractForm {
     }
 
     protected JComponent createFormControl() {
+    	int rowCount = 0;
         TableFormBuilder formBuilder = new TableFormBuilder(getBindingFactory());
-        this.infoField = formBuilder.add("bagName")[1];
+
         formBuilder.row();
-        formBuilder.add("externalDescription");
+        JComponent nameField = formBuilder.add("bagName")[1];
+        JTextField nameTextField = (JTextField) nameField;
+        nameTextField.setEnabled(false);
+        int fieldHeight = nameTextField.getFontMetrics(nameTextField.getFont()).getHeight();
         formBuilder.row();
+        rowCount++;
+//        formBuilder.add("externalDescription");
+/* */
+        JComponent extDesc = formBuilder.addTextArea("externalDescription")[1];
+		((javax.swing.JTextArea) extDesc).setColumns(1);
+		((javax.swing.JTextArea) extDesc).setRows(3);
+		((javax.swing.JTextArea) extDesc).setLineWrap(true);
+/* */
+        formBuilder.row();
+        rowCount++;
+        rowCount++;
         formBuilder.add("baggingDate");
         formBuilder.row();
+        rowCount++;
         formBuilder.add("externalIdentifier");
         formBuilder.row();
+        rowCount++;
         formBuilder.add("bagSize");
         formBuilder.row();
+        rowCount++;
         formBuilder.add("payloadOxum");
         formBuilder.row();
+        rowCount++;
         formBuilder.add("bagGroupIdentifier");
         formBuilder.row();
+        rowCount++;
         formBuilder.add("bagCount");
         formBuilder.row();
+        rowCount++;
         formBuilder.add("internalSenderIdentifier");
         formBuilder.row();
-        formBuilder.add("internalSenderDescription");
+        rowCount++;
+//        formBuilder.add("internalSenderDescription");
+/* */
+        JComponent senderDesc = formBuilder.addTextArea("internalSenderDescription")[1];
+		((javax.swing.JTextArea) senderDesc).setColumns(1);
+		((javax.swing.JTextArea) senderDesc).setRows(3);
+		((javax.swing.JTextArea) senderDesc).setLineWrap(true);
+/* */
+        formBuilder.row();
+        rowCount++;
+        rowCount++;
         if (this.bagView.getBag().getIsEdeposit()) {
-        	formBuilder.row();
         	formBuilder.add("publisher");
+        	formBuilder.row();
+            rowCount++;
         }
         if (this.bagView.getBag().getIsNdnp()) {
-            formBuilder.row();
             formBuilder.add("awardeePhase");        	
+            formBuilder.row();
+            rowCount++;
         }
-        infoField = formBuilder.getForm();
-        infoField.setPreferredSize(dimension);
-        return infoField;
+        infoForm = formBuilder.getForm();
+        int height = 2 * fieldHeight * rowCount;
+        dimension = new Dimension(400, height);
+        infoForm.setPreferredSize(dimension);
+        return infoForm;
     }
 
     public boolean requestFocusInWindow() {
-        return infoField.requestFocusInWindow();
+        return infoForm.requestFocusInWindow();
     }
 
 }
