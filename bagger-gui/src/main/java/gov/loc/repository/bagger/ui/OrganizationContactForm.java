4a41c77f16292a265782f58dcbe09b2ecc6d7d9b
Jonathin Steinbach <jste@rdc.lctl.gov>
2009-04-28 20:14:54 +0000

    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/bagger/trunk@1544 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- bagger-gui/src/main/java/gov/loc/repository/bagger/ui/OrganizationContactForm.java
+++ bagger-gui/src/main/java/gov/loc/repository/bagger/ui/OrganizationContactForm.java
@@ -1,6 +1,8 @@
 
 package gov.loc.repository.bagger.ui;
 
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
@@ -10,7 +12,7 @@ import org.springframework.binding.form.FormModel;
 import org.springframework.richclient.form.AbstractForm;
 import org.springframework.richclient.form.builder.TableFormBuilder;
 
-public class OrganizationContactForm extends AbstractForm implements PropertyChangeListener {
+public class OrganizationContactForm extends AbstractForm implements PropertyChangeListener, FocusListener {
     public static final String CONTACT_FORM_PAGE = "contactPage";
 
     private JComponent contact;
@@ -26,19 +28,34 @@ public class OrganizationContactForm extends AbstractForm implements PropertyCha
 
         formBuilder.row();
         this.contact = formBuilder.add("contactName")[1];
+        this.contact.addFocusListener(this);
         formBuilder.row();
-        formBuilder.add("telephone");
+        JComponent telephoneField = formBuilder.add("telephone")[1];
+        telephoneField.addFocusListener(this);
         formBuilder.row();
-        formBuilder.add("email");
+        JComponent emailField = formBuilder.add("email")[1];
+        emailField.addFocusListener(this);
+        this.contact.requestFocus();
         return formBuilder.getForm();
     }
 
     public void propertyChange(PropertyChangeEvent evt) {
-        //System.out.println("FF-pce: prop=" + evt.getPropertyName() + ", evt=" + evt);
-        if (bagView != null && !this.hasErrors()) bagView.updatePropButton.setEnabled(true);
+        if (bagView != null && !this.hasErrors()) {
+        	bagView.updatePropButton.setEnabled(true);
+        }
    }
 
     public boolean requestFocusInWindow() {
         return contact.requestFocusInWindow();
+    }
+
+    public void focusGained(FocusEvent evt) {
+    }
+    
+    public void focusLost(FocusEvent evt) {
+    	if (bagView != null && !this.hasErrors() && this.isDirty()) {
+    		// TODO: Activate for Bagger 1.6
+    		//bagView.updateBagInfo();
+    	}
     }
 }
