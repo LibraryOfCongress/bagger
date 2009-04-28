a41c7af1035fbd241d4ec8ade0050522233e74ee
Jonathin Steinbach <jste@rdc.lctl.gov>
2009-04-28 20:13:57 +0000

    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/bagger/trunk@1542 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- bagger-gui/src/main/java/gov/loc/repository/bagger/ui/OrganizationGeneralForm.java
+++ bagger-gui/src/main/java/gov/loc/repository/bagger/ui/OrganizationGeneralForm.java
@@ -1,6 +1,8 @@
 
 package gov.loc.repository.bagger.ui;
 
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
@@ -10,7 +12,7 @@ import org.springframework.binding.form.FormModel;
 import org.springframework.richclient.form.AbstractForm;
 import org.springframework.richclient.form.builder.TableFormBuilder;
 
-public class OrganizationGeneralForm extends AbstractForm implements PropertyChangeListener {
+public class OrganizationGeneralForm extends AbstractForm implements PropertyChangeListener, FocusListener {
     public static final String GENERAL_FORM_PAGE = "generalPage";
 
     private JComponent orgField;
@@ -26,18 +28,31 @@ public class OrganizationGeneralForm extends AbstractForm implements PropertyCha
 
         formBuilder.row();
         this.orgField = formBuilder.add("orgName")[1];
+        this.orgField.addFocusListener(this);
         formBuilder.row();
-        formBuilder.add("orgAddress");
+        JComponent orgAddress = formBuilder.add("orgAddress")[1];
+        orgAddress.addFocusListener(this);
+        this.orgField.requestFocus();
         return formBuilder.getForm();
     }
 
     public void propertyChange(PropertyChangeEvent evt) {
-        //System.out.println("FF-pce: prop=" + evt.getPropertyName() + ", evt=" + evt);
-        if (bagView != null && !this.hasErrors()) bagView.updatePropButton.setEnabled(true);
+        if (bagView != null && !this.hasErrors()) {
+        	bagView.updatePropButton.setEnabled(true);
+        }
     }
 
     public boolean requestFocusInWindow() {
         return orgField.requestFocusInWindow();
     }
 
+    public void focusGained(FocusEvent evt) {
+    }
+    
+    public void focusLost(FocusEvent evt) {
+    	if (bagView != null && !this.hasErrors() && this.isDirty()) {
+    		// TODO: Activate for Bagger 1.6
+    		//bagView.updateBagInfo();
+    	}
+    }
 }
