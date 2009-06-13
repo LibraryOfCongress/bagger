1fe96194b84adc8592366c39db6f0223b0fdc4dc
Jonathin Steinbach <jste@rdc.lctl.gov>
2009-06-13 13:02:44 +0000

    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/bagger/trunk@1745 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- bagger-standalone/makeSigned.bat
+++ bagger-standalone/makeSigned.bat
@@ -1,1 +1,1 @@
-jarsigner -keystore bagger.ks -storepass bagger-rdc -keypass bagger-rdc -signedjar signed\bagger.jar target\bagger-standalone-1.5.jar rdc
+jarsigner -keystore bagger.ks -storepass bagger-rdc -keypass bagger-rdc -signedjar signed\bagger.jar target\bagger-standalone-1.6.jar rdc
