24f8d2b90a4ede978ef59df2806353ee7357a750
Jonathin Steinbach <jste@rdc.lctl.gov>
2009-08-27 20:28:31 +0000

    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/bagger/trunk@2088 b77ddd6a-a23c-0410-94b0-b3c43762c485

---
+++ bagger-business/src/main/java/gov/loc/repository/bagger/ProjectProfile.java
@@ -0,0 +1,72 @@
+package gov.loc.repository.bagger;
+
+public class ProjectProfile {
+	private int id = -1;
+	private int projectId;
+	private String fieldName = "";
+	private String fieldValue = "";
+	private boolean isRequired;
+	private boolean isValueRequired;
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setProjectId(int id) {
+		this.projectId = id;
+	}
+
+	public int getProjectId() {
+		return this.projectId;
+	}
+
+	public void setFieldName(String s) {
+		this.fieldName = s;
+	}
+
+	public String getFieldName() {
+		return this.fieldName;
+	}
+
+	public void setIsRequired(boolean b) {
+		this.isRequired = b;
+	}
+
+	public boolean getIsRequired() {
+		return this.isRequired;
+	}
+
+	public void setFieldValue(String s) {
+		this.fieldValue = s;
+	}
+
+	public String getFieldValue() {
+		return this.fieldValue;
+	}
+
+	public void setIsValueRequired(boolean b) {
+		this.isValueRequired = b;
+	}
+
+	public boolean getIsValueRequired() {
+		return this.isValueRequired;
+	}
+
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("Id: ");
+		sb.append(this.getId());
+		sb.append('\n');
+		sb.append("Project Id: ");
+		sb.append(this.projectId);
+		sb.append('\n');
+		sb.append(this.fieldName + '=' + this.fieldValue);
+		sb.append('\n');
+
+		return sb.toString();
+	}
+}
