8345af1dfe3ce27ee35fd3188a67d15693128361
Jonathin Steinbach <jste@rdc.lctl.gov>
2009-09-16 09:00:48 +0000

    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/bagger/trunk@2223 b77ddd6a-a23c-0410-94b0-b3c43762c485

---
+++ bagger-gui/src/main/java/gov/loc/repository/bagger/ui/handlers/RemoveTagFileHandler.java
@@ -0,0 +1,69 @@
+
+package gov.loc.repository.bagger.ui.handlers;
+
+import gov.loc.repository.bagger.bag.impl.DefaultBag;
+import gov.loc.repository.bagger.ui.BagView;
+import gov.loc.repository.bagit.Bag;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreePath;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class RemoveTagFileHandler extends AbstractAction {
+	private static final Log log = LogFactory.getLog(RemoveTagFileHandler.class);
+   	private static final long serialVersionUID = 1L;
+	BagView bagView;
+	DefaultBag bag;
+
+	public RemoveTagFileHandler(BagView bagView) {
+		super();
+		this.bagView = bagView;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+    		removeTagFile();
+	}
+
+    public void removeTagFile() {
+    	String message = "";
+    	this.bag = bagView.getBag();
+    	Bag b = bag.getBag();
+
+    	TreePath[] paths = bagView.bagTagFileTree.getSelectionPaths();
+    	if (paths != null) {
+    		DefaultTreeModel model = (DefaultTreeModel)bagView.bagTagFileTree.getModel();
+        	for (int i=0; i < paths.length; i++) {
+        		TreePath path = paths[i];
+        		Object node = path.getLastPathComponent();
+    			try {
+            		if (node != null) {
+                		if (node instanceof MutableTreeNode) {
+                    		b.removeBagFile(node.toString());
+            				model.removeNodeFromParent((MutableTreeNode)node);
+            			} else {
+                    		b.removeBagFile((String)node);
+            				DefaultMutableTreeNode aNode = new DefaultMutableTreeNode(node);
+            				model.removeNodeFromParent((MutableTreeNode)aNode);
+            			}
+            		}
+    			} catch (Exception e) {
+            	    message += "Error trying to remove file: " + node + "\n";
+            	    bagView.showWarningErrorDialog("Error - file not removed", "Error trying to remove file: " + node + "\n" + e.getMessage());
+    			}
+        	}
+    		bag.isCompleteChecked(false);
+            bag.isValidChecked(false);
+            bagView.setBag(bag);
+            bagView.compositePane.updateCompositePaneTabs(bag, "Tag file removed.");
+        	bagView.bagTagFileTree.removeSelectionPaths(paths);
+        	bagView.bagTagFileTreePanel.refresh(bagView.bagTagFileTree);
+    	}
+	}
+}
