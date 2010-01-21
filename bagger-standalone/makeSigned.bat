2fe17cfbca249d829c3b7d6279702fb74a63eb09
Praveen Bokka <pbok@rdc.lctl.gov>
2010-01-21 06:02:55 +0000

    Bagger 1.11 released. World Digital Library Project profile has been created.
    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/bagger/trunk@2762 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- bagger-standalone/makeSigned.bat
+++ bagger-standalone/makeSigned.bat
@@ -1,1 +1,1 @@
-jarsigner -keystore baggerkeystore.ks -storepass baggerrdc -keypass baggerrdc -signedjar signed\bagger.jar target\bagger-standalone-1.11-SNAPSHOT.jar rdc
+jarsigner -keystore baggerkeystore.ks -storepass baggerrdc -keypass baggerrdc -signedjar signed\bagger.jar target\bagger-standalone-1.11.jar rdc
