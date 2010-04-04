5323198487b7db495dd5b2725c0178129ef78d1a
Praveen Bokka <pbok@rdc.lctl.gov>
2010-04-04 15:57:43 +0000

    Bagger 2.0-SNAPSHOT.
    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/bagger/trunk@3044 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- bagger-standalone/makeSigned.bat
+++ bagger-standalone/makeSigned.bat
@@ -1,1 +1,1 @@
-jarsigner -keystore baggerkeystore.ks -storepass baggerrdc -keypass baggerrdc -signedjar signed\bagger.jar target\bagger-standalone-1.12.jar rdc
+jarsigner -keystore baggerkeystore.ks -storepass baggerrdc -keypass baggerrdc -signedjar signed\bagger.jar target\bagger-standalone-2.0-SNAPSHOT.jar rdc
