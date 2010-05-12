8c0b61b549f5a1b6bd4e8693032d9a003f4f75fb
Praveen Bokka <pbok@rdc.lctl.gov>
2010-05-12 19:10:28 +0000

    Changes from Bagger 2.0 to Bagger 2.1-SNAPSHOT.
    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/bagger/trunk@3229 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- bagger-standalone/makeSigned.bat
+++ bagger-standalone/makeSigned.bat
@@ -1,1 +1,1 @@
-jarsigner -keystore baggerkeystore.ks -storepass baggerrdc -keypass baggerrdc -signedjar signed\bagger.jar target\bagger-standalone-2.0.jar rdc
+jarsigner -keystore baggerkeystore.ks -storepass baggerrdc -keypass baggerrdc -signedjar signed\bagger.jar target\bagger-standalone-2.1-SNAPSHOT.jar rdc
