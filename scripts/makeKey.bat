d67919dd3c3c892c2fdc04bfa0ac963cea41cb28
Salim Malik <smalik@loc.gov>
2011-04-05 15:47:43 +0000

    refs #8
    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/trunk@4411 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- scripts/makeKey.bat
+++ scripts/makeKey.bat
@@ -1,1 +1,1 @@
-keytool -genkeypair -dname "cn=Bagger, ou=RDC, o=Library of Congress, c=US" -alias rdc -keypass baggerrdc -keystore baggerkeystore -storepass baggerrdc
+keytool -genkeypair -dname "cn=Bagger, ou=RDC, o=Library of Congress, c=US" -alias rdc -keypass bagger-rdc -keystore bagger.ks -storepass bagger-rdc
