9811b3765bc75faa8d80e67f377d0f2a506efa98
Salim Malik <smalik@loc.gov>
2011-04-07 17:58:23 +0000

    closes #10
    git-svn-id: https://svn.rdc.lctl.gov/svn/bagger/trunk@4416 b77ddd6a-a23c-0410-94b0-b3c43762c485

--- scripts/buildBaggerDistribution.sh
+++ scripts/buildBaggerDistribution.sh
@@ -1,5 +1,17 @@
-cd ../bagger-maven
-mvn clean install
-cp ../bagger-package/target/bagger ../../bagger_distribution/bagger-2.1.1.jar
-zip bagger_distribution
+#!/bin/bash
 
+# Check out Bagger 2.1.1 from Transfer RDC repositiory
+svn export https://rdc.lctl.gov/svn/bagger/tags/2.1.1 bagger-2.1.1
+
+# Build Bagger jar
+cd bagger-2.1.1_src/bagger-maven
+mvn clean install
+
+# Copy Bagger jar to bagger_distribution folder and rename it to bagger-2.1.1.jar
+cd ../bagger_distribution
+cp bagger-package/target/bagger-package-2.1.1-SNAPSHOT.jar bagger-2.1.1.jar
+
+# Create a copy of the bagger_distribution folder (i.e. bagger-2.1.1)
+cd ..
+cp -r bagger_distribution bagger-2.1.1
+zip -r bagger-2.1.1.zip bagger-2.1.1
